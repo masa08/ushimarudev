c0656928de4e3bdc8c2f9c63c301518e843edeb1
masa08 <ushimasarug08@gmail.com>
2023-02-11 13:44:23 +0900

    delete unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import Footer from '@/features/footer/footer';
+import Header from '@/features/header/header';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function Post() {
+  return <p>post example</p>;
+}
