f1ec25fad7dad1658bfc2d8f4ca4198b89572e60
masa08 <ushimasarug08@gmail.com>
2023-02-12 16:07:24 +0900

    add link to ushimarrudev

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header className="flex justify-between p-5">
       <div>
         <nav>
           <h1>
-            <a href="" className="py-2">
+            <Link href={`/`} className="py-2">
               ushimaru.dev
-            </a>
+            </Link>
           </h1>
           {/* TODO: darkmode切り替え */}
           {/* TODO: 言語切り替え */}
