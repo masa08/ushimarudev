01ee1e3e5599476d632160ddd73da80282030f2b
masa08 <ushimasarug08@gmail.com>
2024-01-31 10:17:21 +0900

    add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,16 @@ import matter from 'gray-matter';
 // TODO: Get data from api
 export async function getPosts(page: string | undefined) {
   let currentOffset = 0;
-  let currentLimit = 10;
-  if (page) {
-    currentOffset = (parseInt(page) - 1) * 10;
-    currentLimit = parseInt(page) * 10;
+  const limit = 10;
+  const pageInt = parseInt(page ?? '1');
+  if (pageInt > 1) {
+    currentOffset = (pageInt - 1) * limit;
   }
 
   const files = fs.readdirSync('src/contents');
-
   const allPosts = files.map((fileName) => {
     const slug = fileName.replace(/\.md$/, '');
     const fileContent = fs.readFileSync(`src/contents/${fileName}`, 'utf-8');
-
     const { data } = matter(fileContent);
     return { metaData: data, slug };
   });
@@ -25,21 +23,20 @@ export async function getPosts(page: string | undefined) {
     return a.metaData.createdAt < b.metaData.createdAt ? 1 : -1;
   });
 
-  const posts = allPosts.slice(currentOffset, currentLimit);
-
-  return posts;
+  const posts = allPosts.slice(currentOffset, currentOffset + limit);
+  const hasMore = currentOffset + limit < allPosts.length;
+  return { posts: posts, hasMore: hasMore };
 }
 
-// TODO: paginatin uiの実装
 const Home = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
   const { page } = searchParams;
-  const posts = await getPosts(page);
+  const { posts, hasMore } = await getPosts(page);
 
-  return <TopTemplate posts={posts} />;
+  return <TopTemplate posts={posts} hasMore={hasMore} />;
 };
 
 export default Home;
